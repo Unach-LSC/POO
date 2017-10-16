8de2e15e9c55e4b5e44700a0aee92478c7807a63
RigobertoPO <rperez@cobach.edu.mx>
2017-10-16 08:52:37 -0500

    v2

---
+++ Empleado.cs
@@ -0,0 +1,12 @@
+using System;
+namespace POO
+{
+    public class Empleado
+    {
+     private int id;
+     private string nombre;
+     private DateTime fechaNacimiento;
+     private double sueldoDiario;
+
+    }
+}
